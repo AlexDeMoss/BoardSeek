cbecda8eb745faa628a21888d147153fcc161040
Rex Douglas <alexanderdemoss@gmail.com>
2019-09-29 22:10:57 -0400

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BoardSeek.Startup))]
+namespace BoardSeek
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
